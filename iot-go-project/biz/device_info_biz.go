d91910bc72f6ab68d7024a46e3d9719532acd0b6
Zen Huifer <huifer97@163.com>
2024-07-18 13:18:48 +0800

    fix: 增加离线逻辑

--- iot-go-project/biz/device_info_biz.go
+++ iot-go-project/biz/device_info_biz.go
@@ -1,9 +1,12 @@
 package biz
 
 import (
+	"context"
+	"encoding/json"
 	"igp/glob"
 	"igp/models"
 	"igp/servlet"
+	"strconv"
 )
 
 type DeviceInfoBiz struct{}
@@ -35,8 +38,33 @@ func (biz *DeviceInfoBiz) PageData(sn string, page, size int) (*servlet.Paginati
 }
 
 func (biz *DeviceInfoBiz) FindById(id uint) *models.DeviceInfo {
+	redis := biz.FindByIdWithRedis(id)
+	if redis != nil {
+		return redis
+	}
+
 	var dt models.DeviceInfo
 	db := glob.GDb
 	db.Where("id = ?", id).Find(&dt)
+	biz.SetRedis(dt)
 	return &dt
+}
+
+func (biz *DeviceInfoBiz) FindByIdWithRedis(id uint) *models.DeviceInfo {
+	val := glob.GRedis.HGet(context.Background(), "struct:device_info", strconv.Itoa(int(id))).Val()
+
+	var res models.DeviceInfo
+	err := json.Unmarshal([]byte(val), &res)
+	if err != nil {
+		return nil
+	}
+	return &res
+}
+
+func (biz *DeviceInfoBiz) SetRedis(newV models.DeviceInfo) {
+	jsonData, _ := json.Marshal(newV)
+	glob.GRedis.HSet(context.Background(), "struct:device_info", strconv.Itoa(int(newV.ID)), jsonData)
+}
+func (biz *DeviceInfoBiz) RemoveRedis(id uint) {
+	glob.GRedis.HDel(context.Background(), "struct:device_info", strconv.Itoa(int(id)))
 }
