2498805c385dcbb705f78eb0f1ca2fb5826824d6
szw <1639914395@qq.com>
2024-07-24 22:01:00 +0800

    fix:增加日志

--- go-iot-mq/handler_storage.go
+++ go-iot-mq/handler_storage.go
@@ -93,6 +93,7 @@ func HandlerDataStorageString(d amqp.Delivery) {
 func StorageDataRowList(dt DataRowList) {
 	signal2 := GetMqttClientSignal2(dt.DeviceUid)
 	zap.S().Infof("获取的mqtt信号数据signal2: %+v", signal2)
+	zap.S().Infof("当前的DataRowList数据: %+v", dt)
 	timeFromUnix := time.Unix(dt.Time, 0)
 	p := influxdb2.NewPointWithMeasurement(dt.DeviceUid).
 		AddField("storage_time", time.Now().Unix()).
@@ -116,25 +117,30 @@ func StorageDataRowList(dt DataRowList) {
 			zap.S().Infof("当前signal_delay_warning的大小: %+v", currentSize)
 			// 如果 ZSet 的大小已经达到或超过配置的缓存大小，则移除第一个元素
 			if currentSize >= signal2[row.Name].CacheSize {
+				zap.S().Infof("当前signal_delay_warning的currentSize大于等于配置大小:%+v", signal2[row.Name].CacheSize)
 				// 移除 ZSet 中分数最低的元素，即最早的元素
 				i := signal2[row.Name].CacheSize + 1 - currentSize
+				zap.S().Infof("计算后的i: %+v", i)
 				if i == 1 {
-
+					zap.S().Infof("计算后的i的值为1")
 				} else {
+					zap.S().Infof("开始移除之前的元素")
 					err := globalRedisClient.ZRemRangeByRank(context.Background(), "signal_delay_warning:"+dt.DeviceUid+":"+strconv.Itoa(signal2[row.Name].ID), 0, i).Err()
 					if err != nil {
 						// 处理错误
 						zap.S().Errorf("移除 ZSet 元素异常：%+v", err)
 					}
 				}
+			} else {
+				zap.S().Infof("当前大小未超过配置大小,写入缓存")
+				// 写入缓存
+				err := globalRedisClient.ZAdd(context.Background(), "signal_delay_warning:"+dt.DeviceUid+":"+strconv.Itoa(signal2[row.Name].ID), redis.Z{Score: float64(dt.Time), Member: row.Value}).Err()
+				if err != nil {
+					// 处理错误
+					zap.S().Errorf("写入 ZSet 元素异常：%+v", err)
+				}
 			}
 
-			// 写入缓存
-			err := globalRedisClient.ZAdd(context.Background(), "signal_delay_warning:"+dt.DeviceUid+":"+strconv.Itoa(signal2[row.Name].ID), redis.Z{Score: float64(dt.Time), Member: row.Value}).Err()
-			if err != nil {
-				// 处理错误
-				zap.S().Errorf("写入 ZSet 元素异常：%+v", err)
-			}
 		}
 
 	}
