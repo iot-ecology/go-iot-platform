6f6e4605bbe8d12cfc9ee7b66041cd335ecb224c
Zen Huifer <huifer97@163.com>
2024-07-26 09:03:28 +0800

    fix: MQTT 通配符订阅处理

--- iot-go-project/servlet/servlet.go
+++ iot-go-project/servlet/servlet.go
@@ -36,7 +36,7 @@ type MqttScript struct {
 type DataRowList struct {
 	Time      int64  `json:"time"`       // 秒级时间戳
 	DeviceUid string `json:"device_uid"` // 是MqttClient的ID
-
+	IdentificationCode string `json:"identification_code"` // 设备标识码
 	DataRows []DataRow `json:"data"`
 	Nc       string    `json:"nc"`
 }
