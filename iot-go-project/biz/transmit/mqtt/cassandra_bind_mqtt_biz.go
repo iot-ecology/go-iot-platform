ff31900e5c0c69d72edee06cd59f89566ea15792
Zen Huifer <huifer97@163.com>
2024-08-01 11:03:31 +0800

    doc: MQTT客户端管理方案

--- iot-go-project/biz/transmit/mqtt/cassandra_bind_mqtt_biz.go
+++ iot-go-project/biz/transmit/mqtt/cassandra_bind_mqtt_biz.go
@@ -35,6 +35,13 @@ func (biz *CassandraTransmitBindBiz) PageData(table string, page, size int) (*se
 	return &pagination, nil
 }
 
+// Bind 方法将CassandraTransmitBind请求绑定到Redis缓存中
+//
+// 参数：
+// req models.CassandraTransmitBind - CassandraTransmitBind请求参数
+//
+// 返回值：
+// 无
 func (biz *CassandraTransmitBindBiz) Bind(req models.CassandraTransmitBind) {
 	if req.Enable == true {
 		jsonData := biz.toByte(req)
@@ -43,6 +50,13 @@ func (biz *CassandraTransmitBindBiz) Bind(req models.CassandraTransmitBind) {
 	}
 }
 
+// toByte 将CassandraTransmitBind请求转换为字节数组
+//
+// 参数：
+//     req models.CassandraTransmitBind - CassandraTransmitBind类型的请求参数
+//
+// 返回值：
+//     []byte - 转换后的字节数组
 func (biz *CassandraTransmitBindBiz) toByte(req models.CassandraTransmitBind) []byte {
 	var ref models.CassandraTransmit
 
@@ -71,6 +85,13 @@ func (biz *CassandraTransmitBindBiz) ChangeEnable(req models.CassandraTransmitBi
 	biz.HandlerRedis(req)
 }
 
+// HandlerRedis 处理CassandraTransmitBind请求中的Redis操作
+//
+// 参数：
+// req models.CassandraTransmitBind - CassandraTransmitBind类型的请求参数
+//
+// 返回值：
+// 无
 func (biz *CassandraTransmitBindBiz) HandlerRedis(req models.CassandraTransmitBind) {
 	if req.Enable == false {
 		glob.GRedis.LRem(context.Background(), "transmit:cassandra:"+req.DeviceUid +":" +req.IdentificationCode, 1, biz.toByte(req))
