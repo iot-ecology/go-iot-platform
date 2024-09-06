079f67f4ea7a67b678b15cfdb2a5df79d24ee44a
Zen Huifer <huifer97@163.com>
2024-09-06 16:07:00 +0800

    feat: react 基础程序准备

---
+++ ant-react/src/pages/notice/dingding/index.tsx
@@ -0,0 +1,17 @@
+import {PageContainer} from '@ant-design/pro-components';
+import React from 'react';
+import {Card} from 'antd';
+import {useIntl} from '@umijs/max';
+
+
+const Admin: React.FC = () => {
+  const intl = useIntl();
+  return (<PageContainer
+
+  >
+    <Card>
+    </Card>
+  </PageContainer>);
+};
+
+export default Admin;
