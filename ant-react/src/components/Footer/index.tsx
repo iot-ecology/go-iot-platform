8109f9353da0c5c22797dd34659db47f63b7fa15
Zen Huifer <huifer97@163.com>
2024-09-19 13:15:12 +0800

    fix: menu 国际化

--- ant-react/src/components/Footer/index.tsx
+++ ant-react/src/components/Footer/index.tsx
@@ -3,11 +3,14 @@ import { DefaultFooter } from '@ant-design/pro-components';
 import React from 'react';
 
 const Footer: React.FC = () => {
+  const currentYear = new Date().getFullYear();
+  const defaultMessage = "Zen HuiFer"
   return (
     <DefaultFooter
       style={{
         background: 'none',
       }}
+      copyright={`${currentYear} ${defaultMessage}`}
       links={[
         {
           key: 'Go IoT 开发平台',
